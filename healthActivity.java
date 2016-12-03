64af4e3d3b1177335c42962d942857850a6f9483
LEEsssshhhhhh <lch0402@nate.com>
2016-12-04 05:48:36 +0900

    보건관 java 소스입니다.

---
+++ healthActivity.java
@@ -0,0 +1,15 @@
+package npp.a1234567;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class healthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health);
+
+    }
+
+}
